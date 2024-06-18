1ec7639fa8f8a5d3816862c9ffd1738d00c07c92
Alexios-hub <528754347@qq.com>
2024-06-18 23:00:48 +0800

    updated

---
+++ CLIP-KD/src/training/gen_aug_feat.py
@@ -0,0 +1,174 @@
+import os
+from typing import List
+os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+
+import open_clip
+import torch
+import torchvision.transforms as transforms
+import webdataset as wds
+from tqdm import tqdm
+import json
+
+import torch
+from torchvision import transforms
+import webdataset as wds
+from training.data import log_and_continue
+from braceexpand import braceexpand
+
+from concurrent.futures import ProcessPoolExecutor
+from torch import Tensor
+from torchvision.transforms import RandAugment,RandomResizedCrop, InterpolationMode
+from torchvision.transforms import functional as F
+from torchvision.transforms.autoaugment import _apply_op
+from open_clip import create_model_and_transforms, get_tokenizer
+import gzip
+from io import BytesIO
+
+
+    
+class myRandAugment(RandAugment):
+    def __init__(self, num_ops=2, magnitude=9, num_magnitude_bins=31, interpolation=InterpolationMode.NEAREST, fill=None):
+        super().__init__(num_ops, magnitude, num_magnitude_bins, interpolation, fill)
+
+    def forward(self, img):
+        fill = self.fill
+        channels, height, width = F.get_dimensions(img)
+        if isinstance(img, Tensor):
+            if isinstance(fill, (int, float)):
+                fill = [float(fill)] * channels
+            elif fill is not None:
+                fill = [float(f) for f in fill]
+
+        op_meta = self._augmentation_space(self.num_magnitude_bins, (height, width))
+        augment_params = []
+        for _ in range(self.num_ops):
+            op_index = int(torch.randint(len(op_meta), (1,)).item())
+            op_name = list(op_meta.keys())[op_index]
+            magnitudes, signed = op_meta[op_name]
+            magnitude = float(magnitudes[self.magnitude].item()) if magnitudes.ndim > 0 else 0.0
+            if signed and torch.randint(2, (1,)):
+                magnitude *= -1.0
+            img = _apply_op(img, op_name, magnitude, interpolation=self.interpolation, fill=fill)
+            augment_params.append((op_index, magnitude))
+
+        return img, augment_params
+    
+
+
+class myRandomResizedCrop(RandomResizedCrop):
+    def __init__(self, size, scale=(0.08, 1.0), ratio=(3/4, 4/3), interpolation=InterpolationMode.BILINEAR):
+        super().__init__(size, scale, ratio, interpolation)
+
+    def forward(self, img):
+        i, j, h, w = self.get_params(img, self.scale, self.ratio)
+        return F.resized_crop(img, i, j, h, w, self.size, self.interpolation), [i, j, h, w]
+
+    
+
+def process(device_id, idx, url, output, maxcount=999999999, batch_size=600):
+    torch.cuda.set_device(device_id)
+    device = torch.device(f'cuda:{device_id}')
+    
+    # Initialize models
+    openai_clip, openai_preprocess_train, openai_preprocess_val = create_model_and_transforms(
+        model_name="ViT-L-14",
+        pretrained='openai',
+        precision="bf16"
+    )
+    openai_clip = openai_clip.to(device)
+    
+    datacomp_clip, datacomp_preprocess_train, datacomp_preprocess_val = create_model_and_transforms(
+        model_name="ViT-L-14",
+        pretrained='/home/user/data/LightClip/CLIP-KD/pretrained_models/CLIP-ViT-L-14-DataComp.XL-s13B-b90K/open_clip_pytorch_model.bin',
+        precision="bf16"
+    )
+    datacomp_clip = datacomp_clip.to(device)
+
+    preprocess = openai_preprocess_val
+
+    # Load dataset
+    src = wds.WebDataset(url) \
+        .decode("pilrgb", handler=log_and_continue) \
+        .to_tuple("__key__", "jpg;png;jpeg;webp", "json", "txt", "syn_caps.json")
+    src = src.batched(batch_size)
+
+    random_crop = myRandomResizedCrop(size=(224, 224))
+    rand_augment = myRandAugment()
+
+    with wds.TarWriter(output) as dst:
+        
+        for batch in tqdm(src, desc=f"Processing Images on GPU {device_id}, Process {idx}"):
+            keys, images, jsons, texts, syn_caps = batch
+        
+            # Prepare batches
+            augmented_images_batch = []
+            all_augmentation_params = []
+            for image in images:
+                for _ in range(5):
+                    cropped_img, crop_params = random_crop(image)
+                    augmented_img, augment_params = rand_augment(cropped_img)
+                    augmented_images_batch.append(preprocess(augmented_img))
+                    all_augmentation_params.append([crop_params, augment_params])
+
+            augmented_images_batch = torch.stack(augmented_images_batch).to(device).to(torch.bfloat16)
+        
+            # Batch inference for images
+            with torch.no_grad():
+                openai_embs = openai_clip.encode_image(augmented_images_batch).cpu().detach()
+                datacomp_embs = datacomp_clip.encode_image(augmented_images_batch).cpu().detach()
+        
+            image_embs = [torch.cat((openai_emb, datacomp_emb), dim=-1) for openai_emb, datacomp_emb in zip(openai_embs, datacomp_embs)]
+        
+            tokenizer = get_tokenizer(model_name="ViT-L-14")
+            text_input = texts + [syn_cap for sublist in syn_caps for syn_cap in sublist]
+            text_input_batch = tokenizer(text_input).to(device)
+        
+            # Batch inference for texts
+            with torch.no_grad():
+                openai_text_embs = openai_clip.encode_text(text_input_batch).cpu().detach()
+                datacomp_text_embs = datacomp_clip.encode_text(text_input_batch).cpu().detach()
+
+            text_embs = [torch.cat((openai_text_emb,datacomp_text_emb),dim=-1) for openai_text_emb, datacomp_text_emb in zip(openai_text_embs, datacomp_text_embs)]
+        
+
+            for i, key in enumerate(keys):
+                sample = {
+                    "__key__": key,
+                    "jpg": images[i],
+                    "syn_caps.json": json.dumps(syn_caps[i]),
+                    "json": jsons[i],
+                    "txt": texts[i],
+                    "paug.json": json.dumps(all_augmentation_params[i*5:(i+1)*5]),
+                    "pth": {
+                        "image_emb": image_embs[i*5:(i+1)*5],
+                        "text_emb": text_embs[i*4:(i+1)*4]
+                    }
+                }
+                dst.write(sample)
+    del openai_clip
+    del datacomp_clip
+    torch.cuda.empty_cache()
+
+def dr_aug_emb():
+    num_gpus = 2  # 有两个GPU
+    models_per_gpu = 1  # 每个GPU运行1个模型实例
+    input_shards = braceexpand("{00416..00830}")#{00000..00415},{00416..00830},{00831..01242}
+    output_shards = braceexpand("{00416..00830}")
+    inputs = [f"/home/user/data/cc12m_sync/{shard}.tar" for shard in input_shards]
+    outputs = [f"/home/user/data/cc12m_dr/{shard}.tar" for shard in output_shards]
+
+
+    with ProcessPoolExecutor(max_workers=num_gpus * models_per_gpu) as executor:
+        futures = []
+        for i in range(len(inputs)):
+            device_id = i % num_gpus
+            proc_idx = i % models_per_gpu
+            futures.append(executor.submit(process, device_id, proc_idx, inputs[i], outputs[i], batch_size=300))
+        
+        for future in tqdm(futures, desc="Total Progress"):
+            future.result()
+
+    print('done')
+
+if __name__ == "__main__":
+    dr_aug_emb()
--- CLIP-KD/src/training/make_synthetic_cap.py
+++ CLIP-KD/src/training/make_synthetic_cap.py
@@ -1,206 +1,23 @@
-from io import BytesIO
 import os
 os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
 
 import open_clip
 import torch
-from PIL import Image
-from training.params import parse_args
-from training.data import get_wds_dataset
 import torchvision.transforms as transforms
-import sys
 import webdataset as wds
 from tqdm import tqdm
 import json
 
 import torch
-from torch.utils.data import IterableDataset
 from torchvision import transforms
 import webdataset as wds
-from itertools import islice,repeat
-import numpy as np
-from training.data import filter_no_caption_or_no_image,log_and_continue
-import dask
-from dask import delayed, compute
+from training.data import log_and_continue
 from braceexpand import braceexpand
 
-from concurrent.futures import ProcessPoolExecutor,ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
 
 
-
-
-# def process(model_name, pretrained, device_id, idx, url, output, maxcount=999999999):
-#     torch.cuda.set_device(device_id)  # 明确设置进程的GPU
-#     device = torch.device(f'cuda:{device_id}')
-#     model, _, base_transform = open_clip.create_model_and_transforms(
-#         model_name=model_name,
-#         pretrained=pretrained
-#     )
-#     model = model.to(device)
-
-#     transform = transforms.Compose([
-#         transforms.Lambda(lambda img: img.convert("RGB")),
-#         base_transform
-#     ])
-#     src = wds.WebDataset(url) \
-#         .decode("pilrgb", handler=log_and_continue) \
-#         .to_tuple("__key__", "jpg;png;jpeg;webp", "json", "txt")
-#     with wds.TarWriter(output) as dst:
-#         for key, image, data, text in tqdm(islice(src, 0, maxcount), desc=f"Processing Images on GPU {device_id}, Process {idx}"):
-#             image_input = transform(image).unsqueeze(0).to(device)
-#             image_input = torch.cat([image_input for _ in range(5)], dim=0)
-#             generated = model.generate(image_input, generation_type='top_p', top_p=0.5, temperature=0.9)
-#             syn_caps = {
-#                 'syn_caps': [open_clip.decode(generated[i]).split("<end_of_text>")[0].replace("<start_of_text>", "") for i in range(generated.shape[0])]
-#             }
-#             sample = {
-#                 "__key__": key,
-#                 "jpg": image,
-#                 "syn_caps.json": json.dumps(syn_caps),
-#                 "json": data,
-#                 "txt": text
-#             }
-#             dst.write(sample)
-
-# def aug_syn_cap():
-#     model_name = "coca_ViT-L-14"
-#     pretrained = "mscoco_finetuned_laion2B-s13B-b90k"
-#     num_gpus = 2  # 有两个GPU
-#     models_per_gpu = 11  # 每个GPU运行11个模型实例
-#     input_shards = braceexpand("{00000..01242}")
-#     output_shards = braceexpand("{00000..01242}")
-#     inputs = [f"/home/user/data/cc12m/{shard}.tar" for shard in input_shards]
-#     outputs = [f"/home/user/data/cc12m_sync/{shard}.tar" for shard in output_shards]
-    
-#     with ProcessPoolExecutor(max_workers=num_gpus*models_per_gpu) as executor:
-#         futures = []
-#         for i in range(len(inputs)):
-#             device_id = i % num_gpus
-#             proc_idx = i % models_per_gpu
-#             futures.append(executor.submit(process, model_name, pretrained, device_id, proc_idx, inputs[i], outputs[i]))
-        
-#         for future in tqdm(futures, desc="Total Progress"):
-#             future.result()
-
-#     print('done')
-
-
-# def process(model_name, pretrained, device_id, idx, url, output, maxcount=999999999, batch_size=512):
-#     torch.cuda.set_device(device_id)  # 明确设置进程的GPU
-#     device = torch.device(f'cuda:{device_id}')
-#     model, _, base_transform = open_clip.create_model_and_transforms(
-#         model_name=model_name,
-#         pretrained=pretrained
-#     )
-#     model = model.to(device)
-#     transform = transforms.Compose([
-#         transforms.Lambda(lambda img: img.convert("RGB")),
-#         base_transform
-#     ])
-#     src = wds.WebDataset(url) \
-#         .decode("pilrgb", handler=log_and_continue) \
-#         .to_tuple("__key__", "jpg;png;jpeg;webp", "json", "txt")
-    
-#     src = src.batched(batch_size)
-
-#     for batch in tqdm(src, desc=f"Processing Images on GPU {device_id}, Process {idx}"):
-#         keys, images, jsons, texts = batch
-#         input_images = [transform(image).unsqueeze(0).to(device) for image in images]
-#         input_images = torch.cat(input_images, dim=0)  # 将所有图像合并为一个批处理
-#         input_images = torch.repeat_interleave(input_images, repeats=5, dim=0)  # 每个图像复制5次
-
-#         with torch.no_grad(), torch.cuda.amp.autocast():
-#             generated = model.generate(images, generation_type='top_p', top_p=0.5, temperature=0.9)
-
-#         # 处理每个原始图像的5个captions
-#         syn_caps = []
-#         for i in range(0, generated.shape[0], 5):
-#             captions = [open_clip.decode(generated[j]).split("<end_of_text>")[0].replace("<start_of_text>", "")
-#                         for j in range(i, i+5)]
-#             syn_caps.append(captions)
-
-#         for key, image, syn_cap_group, data, text in zip(keys, images, syn_caps, jsons, texts):
-#             with wds.TarWriter(output) as dst:
-#                 sample = {
-#                     "__key__": key,
-#                     "jpg": image,
-#                     "syn_caps.json": json.dumps(syn_cap_group),
-#                     "json": data,
-#                     "txt": text
-#                 }
-#                 dst.write(sample)
-
-# def process_caption_decoding(start_index, generated, num_captions=5):
-#     captions = [open_clip.decode(generated[j]).split("<end_of_text>")[0].replace("<start_of_text>", "")
-#                 for j in range(start_index, start_index + num_captions)]
-#     return captions
-
-# def write_to_tar(keys, images, syn_caps, jsons, texts, output):
-#     with wds.TarWriter(output) as dst:
-#         for key, image, syn_cap_group, data, text in zip(keys, images, syn_caps, jsons, texts):
-#             sample = {
-#                 "__key__": key,
-#                 "jpg": image,
-#                 "syn_caps.json": json.dumps(syn_cap_group),
-#                 "json": data,
-#                 "txt": text
-#             }
-#             dst.write(sample)
-
-# def process(model_name, pretrained, device_id, idx, url, output, maxcount=999999999, batch_size=512):
-#     torch.cuda.set_device(device_id)  # Set GPU for the process
-#     device = torch.device(f'cuda:{device_id}')
-#     model, _, base_transform = open_clip.create_model_and_transforms(
-#         model_name=model_name,
-#         pretrained=pretrained
-#     )
-#     model = model.to(device)
-#     transform = transforms.Compose([
-#         transforms.Lambda(lambda img: img.convert("RGB")),
-#         base_transform
-#     ])
-#     src = wds.WebDataset(url).decode("pilrgb", handler=log_and_continue).to_tuple("__key__", "jpg;png;jpeg;webp", "json", "txt")
-#     src = src.batched(batch_size)
-
-#     for batch in tqdm(src, desc=f"Processing Images on GPU {device_id}, Process {idx}"):
-#         keys, images, jsons, texts = batch
-#         input_images = [transform(image).unsqueeze(0).to(device) for image in images]
-#         input_images = torch.cat(input_images, dim=0)
-#         input_images = torch.repeat_interleave(input_images, repeats=5, dim=0)
-
-#         with torch.no_grad(), torch.cuda.amp.autocast():
-#             generated = model.generate(input_images, generation_type='top_p', top_p=0.5, temperature=0.9)
-
-#         with ThreadPoolExecutor() as executor:
-#             syn_caps = list(executor.map(process_caption_decoding, range(0, generated.shape[0], 5), repeat(generated)))
-
-
-#         write_to_tar(keys, images, syn_caps, jsons, texts, output)
-
-# def aug_syn_cap():
-#     model_name = "coca_ViT-L-14"
-#     pretrained = "mscoco_finetuned_laion2B-s13B-b90k"
-#     num_gpus = 2  # 有两个GPU
-#     models_per_gpu = 1  # 每个GPU运行1个模型实例
-#     input_shards = braceexpand("{00000..01242}")
-#     output_shards = braceexpand("{00000..01242}")
-#     inputs = [f"/home/user/data/cc12m/{shard}.tar" for shard in input_shards]
-#     outputs = [f"/home/user/data/cc12m_sync/{shard}.tar" for shard in output_shards]
-    
-#     with ProcessPoolExecutor(max_workers=num_gpus * models_per_gpu) as executor:
-#         futures = []
-#         for i in range(len(inputs)):
-#             device_id = i % num_gpus
-#             proc_idx = i % models_per_gpu
-#             futures.append(executor.submit(process, model_name, pretrained, device_id, proc_idx, inputs[i], outputs[i]))
-        
-#         for future in tqdm(futures, desc="Total Progress"):
-#             future.result()
-
-#     print('done')
-
-
-def process(model_name, pretrained, device_id, idx, url, output, maxcount=999999999, batch_size=760):
+def process(model_name, pretrained, device_id, idx, url, output, maxcount=999999999, batch_size=600):
     torch.cuda.set_device(device_id)  # 明确设置进程的GPU
     device = torch.device(f'cuda:{device_id}')
     model, _, base_transform = open_clip.create_model_and_transforms(
@@ -252,8 +69,8 @@ def aug_syn_cap():
     pretrained = "mscoco_finetuned_laion2B-s13B-b90k"
     num_gpus = 2  # 有两个GPU
     models_per_gpu = 1  # 每个GPU运行1个模型实例
-    input_shards = braceexpand("{00306..01242}")
-    output_shards = braceexpand("{00306..01242}")
+    input_shards = braceexpand("{01129..01133}")
+    output_shards = braceexpand("{01129..01133}")
     inputs = [f"/home/user/data/cc12m/{shard}.tar" for shard in input_shards]
     outputs = [f"/home/user/data/cc12m_sync/{shard}.tar" for shard in output_shards]
     
